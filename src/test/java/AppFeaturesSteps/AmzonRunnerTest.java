ee8ab3486dcb6379ce9c76bca3935d1848aa5aa1
suraj <suraj746@gmail.com>
2023-11-17 12:26:28 +0530

    push

--- src/test/java/AppFeaturesSteps/AmzonRunnerTest.java
+++ src/test/java/AppFeaturesSteps/AmzonRunnerTest.java
@@ -7,8 +7,8 @@ import io.cucumber.testng.CucumberOptions;
 		features= {"C:\\Users\\HP\\eclipse-workspace\\ATTSeleniumProject\\src\\test\\resources\\AppFeature"},
 		glue= {"AppFeaturesSteps"},
 		plugin= {"pretty", "html:target/cucumber-report/amzonreport.html",
-				"json:target/cucumber-report.json"},
-		tags= "@smoke"
+				"json:target/cucumber-report.json"}
+		//tags= "@smoke"
 		
 		
 		
